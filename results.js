e7edc7e20ba25a391e7b5802d5a2efd7aa2e2e2c
Alhassan-Jamila <jamilaal865@gmail.com>
2022-04-06 14:57:34 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,14 @@
+const results=[
+{name:"Wendy Dari", class:"Form one", marks:8},
+{name:"Abass Nazifa", class:"Form one", marks:6},
+{name:"Isshaku Shamira", class:"Form one", marks:5},
+{name:"Ali Nuhu", class:"Form one", marks:4},
+{name:"Amadu Ayisha", class:"Form one", marks:3},
+{name:"Abudu Waris", class:"Form one", marks:10},
+{name:"Kufi Aika", class:"Form one", marks:20},
+{name:"Nuri Sala", class:"Form one", marks:30},
+{name:"Dada Baba", class:"Form one", marks:41},
+{name:"Jaliha Wasilas", class:"Form one", marks:15},
+]
+
+module.exports=results
